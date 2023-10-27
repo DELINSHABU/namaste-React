837ad084dd1e6c9226f2bfa7a164b55fd9bfe9d5
DELINSHABU <82861020+DELINSHABU@users.noreply.github.com>
2023-10-27 16:38:01 +0530

    3 video

--- revison/app.js
+++ revison/app.js
@@ -1,2 +1,22 @@
-import React from "react/client"
-import React from "react-dom"
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const headMod = React.createElement("h1",{
+    id: "head",
+    class: "head"
+},"Hello Everyone this h1 tag is render using React")
+
+const paraMod = React.createElement("p",{
+    id: "para",
+    class: "para"
+},"Hello Everyone this p tag is render using React")
+
+const containerMod = React.createElement("p",{
+    id: "cont",
+    class: "cont"
+},[headMod,paraMod])
+
+
+const root =ReactDOM.createRoot(document.getElementById("root"))
+root.render(containerMod)
+
